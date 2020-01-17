71dae4f18a1f8a7a869e0d5749c3ddc2824a5d6a
Daniel Cornelio <jdcornelio89@gmail.com>
2020-01-17 14:56:24 -0600

    trabajando con props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function Hello (props){
+//   return <h2>{props.title}</h2>
+// }
+
+// const Hello = (props)=><h2>{props.title}</h2>
+
+class Hello extends Component{
+  render(){
+    return(
+      <h2>{this.props.title}</h2>
+    )
+  }
+}
+
+class Text extends Component{
+  render(){
+    const seboolean = this.props.boolean ? "Daniel" : "Cornelio"
+    return(
+      <>
+      <p>{this.props.text}</p>
+      <p>{this.props.number}</p>
+      <p>{seboolean}</p>
+      </>
+    )
+  }
+}
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Curso de React js</h1>
+          <Hello
+            title="Hola por props"
+          />
+          <Text
+            text="Desde componente texto"
+            number = {9}
+            boolean = {true}
+          />
+          </header>
+      </div>
+    );
+  }
 }
 
 export default App;
